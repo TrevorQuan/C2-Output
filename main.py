ba405051fd0919c7ebaa794db177667ae8003dfd
Quan Trevor <124255@eesd.org>
2021-01-11 23:58:05 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello World") # This print statement prints out 'Hello World'
+
+# Write 5 print statements
+# Name, grade, favorite color, favorite sport, favorite ice cream flavor
+print("My name is Trevor Quan.")
+print("I am in 7th Grade.")
+print("My favorite color is red.")
+print("My favorite sport is badminton and swimming.")
+print("My favorite ice cream flavor is chocolate.")
